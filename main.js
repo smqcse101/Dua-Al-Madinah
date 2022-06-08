edd89236e0c7c973358e2f9f828c6e9e82a0f99d
Syed Mohammed Quadri <syed.mustafa2k@gmail.com>
2022-06-08 11:11:43 +0530

    changed the call back uri form local host to heroku link

--- main.js
+++ main.js
@@ -90,7 +90,7 @@ passport.deserializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/login",
+    callbackURL: "https://hidden-mesa-15600.herokuapp.com/auth/google/login",
     userProfileUrl:"https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
