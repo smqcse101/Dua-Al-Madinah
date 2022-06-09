396d5afcd76349e7c5886d52b6365118bf1372bd
Syed Mohammed Quadri <syed.mustafa2k@gmail.com>
2022-06-09 18:16:06 +0530

    footer done

--- main.js
+++ main.js
@@ -90,7 +90,9 @@ passport.deserializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
+
     callbackURL: "https://hidden-mesa-15600.herokuapp.com/auth/google/login",
+    // callbackURL: "http://localhost:3000/auth/google/login",
     userProfileUrl:"https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
